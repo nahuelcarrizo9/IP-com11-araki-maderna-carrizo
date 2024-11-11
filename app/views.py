f2753e646e1fdbe1e5fdcc4f85770799b8218011
Nahuel Carrizo <carrizonahue86@gmail.com>
2024-11-11 13:01:36 -0300

    Lista de favoritos(falta agregar la opción para que solo se puedan agregar una sola vez)

--- app/views.py
+++ app/views.py
@@ -12,8 +12,7 @@ def index_page(request):
 # si el opcional de favoritos no está desarrollado, devuelve un listado vacío.
 def home(request):
     images = services.getAllImages()
-    favourite_list = []
-
+    favourite_list = getAllFavouritesByUser(request)
     return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
 
 def search(request):
@@ -31,16 +30,18 @@ def search(request):
 # Estas funciones se usan cuando el usuario está logueado en la aplicación.
 @login_required
 def getAllFavouritesByUser(request):
-    favourite_list = []
+    favourite_list = services.getAllFavourites(request)
     return render(request, 'favourites.html', { 'favourite_list': favourite_list })
 
 @login_required
 def saveFavourite(request):
-    pass
+    services.saveFavourite(request)
+    return redirect('/favourites')
 
 @login_required
 def deleteFavourite(request):
-    pass
+    services.deleteFavourite(request)
+    return redirect('/favourites')
 
 @login_required
 def exit(request):
